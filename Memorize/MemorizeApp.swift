810920e2f445320af16abd6dc17dd7db3d963f16
yakirteng <yakirteng@tencent.com>
2023-07-04 23:17:15 +0800

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,20 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by yakir on 2023/7/4.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
