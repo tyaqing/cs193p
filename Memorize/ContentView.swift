96c2fe139ac946a587cbe2860680d5858bb438c4
yakirteng <yakirteng@tencent.com>
2023-07-05 20:14:31 +0800

    add new swift ui file

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -5,84 +5,49 @@
 //  Created by yakir on 2023/7/4.
 //
 
-import SwiftUI
 import CoreData
+import SwiftUI
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
-    var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
-                    }
-                }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-            Text("Select an item")
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
+	var body: some View {
+		HStack {
+			ForEach(0 ..< 4) { index in
+				CardView(isFaceUp: index % 2 == 0)
+			}
+		}
+		.padding(.horizontal)
+		.foregroundColor(/*@START_MENU_TOKEN@*/ .red/*@END_MENU_TOKEN@*/)
+	}
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
+struct CardView: View {
+	var isFaceUp: Bool
+
+	var body: some View {
+		ZStack(alignment: .center) {
+			if isFaceUp {
+				RoundedRectangle(cornerRadius: 20)
+					.fill()
+					.foregroundColor(.white)
+				RoundedRectangle(cornerRadius: 20)
+					.stroke(lineWidth: 3)
+				Text("👻")
+					.font(.largeTitle)
+			} else {
+				RoundedRectangle(cornerRadius: 20)
+					.fill()
+			}
+		}
+	}
+}
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
-    }
+	static var previews: some View {
+		ContentView()
+			.preferredColorScheme(.dark)
+			.previewDevice("iPhone 14")
+		ContentView()
+			.preferredColorScheme(.light)
+			.previewDevice("iPhone 14")
+	}
 }
