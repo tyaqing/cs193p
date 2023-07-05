10f5e10b2331be1ae1207a75061f2c24bf0a5625
yakirteng <yakirteng@tencent.com>
2023-07-05 19:59:42 +0800

    first commit

---
+++ Memorize/Bak.swift
@@ -0,0 +1,20 @@
+//
+//  Bak.swift
+//  Memorize
+//
+//  Created by yakir on 2023/7/4.
+//
+
+import SwiftUI
+
+struct Bak: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Bak_Previews: PreviewProvider {
+    static var previews: some View {
+        Bak()
+    }
+}
