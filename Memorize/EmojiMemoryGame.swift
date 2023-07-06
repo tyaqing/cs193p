0728df4c26d1456fadcc12b21f4564f9a4abab97
yakirteng <yakirteng@tencent.com>
2023-07-06 20:43:38 +0800

    完成             Lecture 3: MVVM and the Swift type system

--- Memorize/EmojiMemoryGame.swift
+++ Memorize/EmojiMemoryGame.swift
@@ -5,4 +5,20 @@
 //  Created by yakir on 2023/7/6.
 //
 
-import Foundation
+import SwiftUI
+
+class EmojiMemoryGame {
+	static var emojis = ["🚗", "🚕", "🚙", "🚌", "🚎", "🏎", "🚓", "🚑", "🚒", "🚐", "🛻", "🚚", "🚛", "🚜", "🛵", "🏍", "🛺", "🚲", "🛴", "🚔", "🚍", "🚘", "🚖"]
+
+	static func createMemoryGame() -> MemorizeGame<String> {
+		return MemorizeGame(numberOfPairsOfCards: 4) {
+			index in EmojiMemoryGame.emojis[index]
+		}
+	}
+
+	var model: MemorizeGame<String> = createMemoryGame()
+
+	var cards: [MemorizeGame<String>.Card] {
+		return model.cards
+	}
+}
--- Memorize/MemorizeGame.swift
+++ Memorize/MemorizeGame.swift
@@ -1,13 +1,21 @@
 import Foundation
 
 struct MemorizeGame<CardContent> {
-	var card: [Card]
+	private(set) var cards: [Card]
 
 	func choose(_ card: Card) {}
 
+	init(numberOfPairsOfCards: Int, createCardContent: (Int) -> CardContent) {
+		cards = [Card]()
+		for pairIndex in 0 ..< numberOfPairsOfCards {
+			let content = createCardContent(pairIndex)
+			cards.append(Card(content: content))
+		}
+	}
+
 	struct Card {
-		var isFaceUp: Bool
-		var isMatched: Bool
+		var isFaceUp: Bool = false
+		var isMatched: Bool = false
 		var content: CardContent
 	}
 }
