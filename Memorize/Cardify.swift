e6a56b54ebde5b25dc0213a09ed11a99a4b0ecfb
yakirteng <yakirteng@tencent.com>
2023-07-08 17:39:21 +0800

    完成一个ViewModifier

---
+++ Memorize/Cardify.swift
@@ -0,0 +1,29 @@
+import SwiftUI
+
+// ViewModifier 的写法有点类似于vue中的slot 将内容包裹在Card里面
+struct Cardify: ViewModifier {
+	var isFaceUp: Bool
+	func body(content: Content) -> some View {
+		ZStack {
+			let shape = RoundedRectangle(cornerRadius: DrawingConstants.cornerRadius)
+			if self.isFaceUp {
+				shape.fill().foregroundColor(.white)
+				shape.strokeBorder(lineWidth: DrawingConstants.lineWidth)
+				content
+			} else {
+				shape.fill()
+			}
+		}
+	}
+
+	private enum DrawingConstants {
+		static let cornerRadius: CGFloat = 10
+		static let lineWidth: CGFloat = 3
+	}
+}
+
+extension View {
+	func cardify(isFaceUp: Bool) -> some View {
+		self.modifier(Cardify(isFaceUp: isFaceUp))
+	}
+}
--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -36,18 +36,10 @@ struct CardView: View {
 	var body: some View {
 		GeometryReader { geometry in
 			ZStack {
-				let shape = RoundedRectangle(cornerRadius: DrawingConstants.cornerRadius)
-				if card.isFaceUp {
-					shape.fill().foregroundColor(.white)
-					shape.strokeBorder(lineWidth: DrawingConstants.lineWidth)
-					Pie(startAngle: Angle(degrees: 0-90), endAngle: Angle(degrees: 110-90)).padding(DrawingConstants.circlePadding).opacity(0.5)
-					Text(card.content).font(font(in: geometry.size))
-				} else if card.isMatched {
-					shape.opacity(0)
-				} else {
-					shape.fill()
-				}
+				Pie(startAngle: Angle(degrees: 0-90), endAngle: Angle(degrees: 110-90)).padding(DrawingConstants.circlePadding).opacity(0.5)
+				Text(card.content).font(font(in: geometry.size))
 			}
+			.cardify(isFaceUp: card.isFaceUp)
 		}
 	}
 
